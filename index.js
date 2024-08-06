09bafd188ed2d7f90d80d01a2f8fc6403a3c6078
MelissaOrdonez <melissaordo5@tamu.edu>
2024-08-06 16:30:53 -0500

    Added backend to calendar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
-import "./script.js";
+import events from "node:events";
 
 const app = express();
 const port = 5000;
@@ -8,13 +8,6 @@ const port = 5000;
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"));
 
-let session = "";
-app.post("/calendar", (req, res) => {
-    session = req.params.id;
-    console.log(session);
-    res.render('calendar.ejs')
-});
-    
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
@@ -52,9 +45,71 @@ app.get("/other", (req, res) => {
     res.render("portfolio/other.ejs");
 })
 app.get("/calendar", (req, res) => {
+    let session = req.query.submit;
 
-    res.render("calendar.ejs");
+    let currentDate = "", 
+    daysTag = "", 
+    prevNextIcon = "";
+
+    //.icons span
+
+    let date = new Date(),
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+
+    const months = ["January", "Febuary", "March", "April", "May", "June",
+         "July", "August", "September", "October", "November", "December"];
+    
+    const renderCalendar = () => {
+        let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDate(),
+        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+        let liTag = "";
+        let count = 0;
+
+        for(let i = firstDayofMonth; i > 0; i--){
+            liTag += `<li class = "inactive">${lastDateofLastMonth-i+1}</li>`;
+            count += 1;
+        }
+
+        for(let i = 1; i <= lastDateofMonth; i++){
+            let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+            liTag += `<li class ="${isToday}">${i}</li>`;
+        }
+
+        for(let i = lastDayofMonth; i < 35-count; i++){
+            liTag += `<li class = "inactive">${i-lastDayofMonth+1}</li>`;
+        }
+
+        currentDate = `${months[currMonth]} ${currYear}`;
+        daysTag = liTag;
+    }
+    renderCalendar();
+
+    res.render("calendar.ejs", {
+        date : currentDate,
+        days : daysTag
+    });
+
+    app.post("/calendar", (req, res) => {
+        currMonth = req.body.submit === "prev" ? currMonth - 1 : currMonth + 1;
+        if(currMonth < 0 || currMonth > 11){
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        } else{
+            date = new Date();
+        }
+        renderCalendar();
+
+        res.render("calendar.ejs", {
+            date : currentDate,
+            days : daysTag
+        })
+    })
 })
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
--- script.js
+++
@@ -1,6 +0,0 @@
-const currentDate = document.querySelector(".current-date");
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
-
-console.log(date, currYear, currMonth);
